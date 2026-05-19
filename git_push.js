8608cba8ecf19a08bd44fb067cb4d7e0d1245134
Muhammad Anas Qureshi <muhammad.anas.quershi@convergentbt.com>
2026-05-19 18:51:26 +0500

    chore: secure setup API endpoint

--- git_push.js
+++ git_push.js
@@ -3,7 +3,7 @@ try {
     console.log("Staging...");
     execSync('git -C "d:\\Anas\\ZATCA-Universal-Portal-[18-05-2026]" add -A', { stdio: 'inherit' });
     console.log("Committing...");
-    execSync('git -C "d:\\Anas\\ZATCA-Universal-Portal-[18-05-2026]" commit -m "fix: resolve global URL shadowing compiler error"', { stdio: 'inherit' });
+    execSync('git -C "d:\\Anas\\ZATCA-Universal-Portal-[18-05-2026]" commit -m "chore: secure setup API endpoint"', { stdio: 'inherit' });
     console.log("Pushing...");
     execSync('git -C "d:\\Anas\\ZATCA-Universal-Portal-[18-05-2026]" push origin main', { stdio: 'inherit' });
     console.log("All done!");
